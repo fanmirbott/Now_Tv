04877d3efaa3771a2445698b1898338c3b766b48
Saidmurod Norqobilov <saidmurodnorqobilov2005@gmail.com>
2025-07-22 15:20:01 +0500

    Now TV: UI update or fixes

--- lib/OnboardingPage.dart
+++ lib/OnboardingPage.dart
@@ -1,21 +1,81 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(OnboardingPage());
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: OnboardingPage(),
+    ),
+  );
 }
 
 class OnboardingPage extends StatefulWidget {
   const OnboardingPage({super.key});
 
   @override
-  State<OnboardingPage> createState() => _State();
+  State<OnboardingPage> createState() => _OnboardingPageState();
 }
 
-class _State extends State<OnboardingPage> {
+class _OnboardingPageState extends State<OnboardingPage> {
+  late PageController _pageController;
+  int isActiv = 0;
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: 0);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  void nextPage() {
+    if (isActiv < 2) {
+      _pageController.nextPage(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    }
+  }
+
+  List<Map> items = [
+    {
+      "text": "Easy Streaming",
+      "text1": "Choose your plan to watch live match your favourite club.",
+    },
+
+    {
+      "text": "Always Uptodate ",
+      "text1": "Stay updated with match highlight, preview and hot news",
+    },
+    {
+      "text": "Get Amazing Reward",
+      "text1": "Redeem your points to get an amazing reward",
+    },
+  ];
+
+  List<Map> items1 = [
+    {
+      "text": "Next",
+      "text1": "Skip",
+    },
+    {
+      "text": "Next",
+      "text1": "Back",
+    },
+
+    {
+      "text": "Let’s Get Started",
+      "text1": "Back",
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF2D2D2D),
+      backgroundColor: const Color(0xFF2D2D2D),
       body: Stack(
         children: [
           Column(
@@ -24,41 +84,39 @@ class _State extends State<OnboardingPage> {
                 width: double.infinity,
                 height: 363,
                 child: PageView(
+                  controller: _pageController,
                   scrollDirection: Axis.horizontal,
-                  children: [
-                    Image.asset(
-                      "assets/onboardingPage/img.png",
-                      fit: BoxFit.cover,
-                    ),
-                    Image.asset(
-                      "assets/onboardingPage/img_1.png",
-                      fit: BoxFit.cover,
-                    ),
-                    Image.asset(
-                      "assets/onboardingPage/img_2.png",
-                      fit: BoxFit.cover,
-                    ),
+                  onPageChanged: (index) {
+                    setState(() {
+                      isActiv = index;
+                    });
+                  },
+                  children: const [
+                    ImageWidget("assets/onboardingPage/img.png"),
+                    ImageWidget("assets/onboardingPage/img_1.png"),
+                    ImageWidget("assets/onboardingPage/img_2.png"),
                   ],
                 ),
               ),
-              SizedBox(height: 28),
+              const SizedBox(height: 28),
               Container(
                 width: 293,
                 height: 99,
                 child: Column(
                   children: [
                     Text(
-                      "Easy Streaming",
-                      style: TextStyle(
+                      items[isActiv]['text'],
+                      style: const TextStyle(
                         fontSize: 24,
                         fontWeight: FontWeight.w600,
                         color: Color(0xFFFFFFFF),
                       ),
                     ),
-                    SizedBox(height: 18),
+                    const SizedBox(height: 18),
                     Text(
-                      "Choose your plan to watch live match your favourite club.",
-                      style: TextStyle(
+                      items[isActiv]['text1'],
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
                         fontWeight: FontWeight.w500,
                         fontSize: 16,
                         color: Color(0xFFDDDDDD),
@@ -67,78 +125,85 @@ class _State extends State<OnboardingPage> {
                   ],
                 ),
               ),
-              SizedBox(height: 128),
-              Container(
-                width: 40,
-                height: 8,
-                child: Row(
-                  spacing: 8,
-                  children: [
-                    Container(
-                      width: 8,
-                      height: 8,
-                      decoration: BoxDecoration(
-                        color: Color(0xFF777777),
-                        // color: Color(0xFF454343),
-                        shape: BoxShape.circle,
-                      ),
+
+              const SizedBox(height: 128),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(3, (index) {
+                  return Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 4),
+                    width: 8,
+                    height: 8,
+                    decoration: BoxDecoration(
+                      color: isActiv == index
+                          ? const Color(0xFF777777)
+                          : const Color(0xFF454343),
+                      shape: BoxShape.circle,
                     ),
-                    Container(
-                      width: 8,
-                      height: 8,
-                      decoration: BoxDecoration(
-                        color: Color(0xFF454343),
-                        shape: BoxShape.circle,
-                      ),
-                    ),
-                    Container(
-                      width: 8,
-                      height: 8,
-                      decoration: BoxDecoration(
-                        color: Color(0xFF454343),
-                        shape: BoxShape.circle,
-                      ),
-                    ),
-                  ],
-                ),
+                  );
+                }),
               ),
-              SizedBox(height: 148),
-              IconButton(
-                onPressed: () {},
-                style: IconButton.styleFrom(
-                  backgroundColor: Color(0xFF00A3B7),
-                  fixedSize: Size(336, 40),
-                ),
-                icon: Text(
-                  "Next",
-                  style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.w600,
-                    color: Color(0xFFEAEAEA),
+              const SizedBox(height: 170),
+              SizedBox(
+                width: 336,
+                height: 40,
+                child: IconButton(
+                  onPressed: nextPage,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF00A3B7),
+                  ),
+                  icon: Text(
+                    items1[isActiv]["text"],
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w600,
+                      color: Color(0xFFEAEAEA),
+                    ),
                   ),
                 ),
               ),
-              IconButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                style: IconButton.styleFrom(
-                  backgroundColor: Colors.grey,
-                  fixedSize: Size(336, 40),
-                ),
-                icon: Text(
-                  "Skip",
-                  style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.w600,
-                    color: Color(0xFFEAEAEA),
+              const SizedBox(height: 12),
+              SizedBox(
+                width: 336,
+                height: 40,
+                child: IconButton(
+                  onPressed: () {
+                    if (isActiv == 0) {
+                      _pageController.jumpToPage(2); // Skip
+                    } else {
+                      _pageController.previousPage(
+                        duration: Duration(milliseconds: 300),
+                        curve: Curves.easeInOut,
+                      ); // Back
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.grey),
+                  icon: Text(
+                    isActiv == 0 ? "Skip" : "Back",  // Matnni ham moslashtiramiz
+                    style: const TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w600,
+                      color: Color(0xFFEAEAEA),
+                    ),
                   ),
                 ),
               ),
+
             ],
           ),
         ],
       ),
     );
+  }
+}
+
+class ImageWidget extends StatelessWidget {
+  final String path;
+
+  const ImageWidget(this.path, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(path, fit: BoxFit.cover);
   }
 }
